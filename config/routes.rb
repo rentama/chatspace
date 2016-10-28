613d1ab66c381532b497ac55e5d1847ab025b145
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-10-28 14:50:16 +0900

    ミスがあったので修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chat
+  resources :chats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
