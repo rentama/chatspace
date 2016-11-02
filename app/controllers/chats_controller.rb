2cb9d4b7defa686cb0a61c527f7918a122c89cde
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-11-02 16:55:56 +0900

    メッセージ投稿機能の実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,15 @@
 class ChatsController < ApplicationController
   def index
+    @message = Message.new
+  end
+
+  def create
+    Message.create(create_params)
+    redirect_to :action => "index"
+  end
+
+  private
+  def create_params
+      params.require(:message).permit(:body)
   end
 end
