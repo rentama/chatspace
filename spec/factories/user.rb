3d388d5bd79563a2307f4af391b4974b4cf26393
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-11-24 18:22:41 +0900

    FactoryGirlでuserインスタンスを作ってくれるように設定

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name                  "igari"
+    email                 "aaa@gmail.com"
+    password              "12345678"
+    password_confirmation "12345678"
+  end
+
+end
