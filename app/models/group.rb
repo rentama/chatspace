10065bc880934313245f7e45ec7c2cb6673b64a6
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-11-29 16:14:05 +0900

    バリデーション追加、アソシエーションの子要素が保存されるようにした。

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   has_many :messages
+  accepts_nested_attributes_for :users
+
+  validates :name, presence: true
 end
