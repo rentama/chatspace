67ce4ecb266d1ffa27f3abb12ec9d100bbbcb717
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-12-02 19:01:28 +0900

    グループ詳細画面のためのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "chats#index"
   resources :chats, only: [:index, :create]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
