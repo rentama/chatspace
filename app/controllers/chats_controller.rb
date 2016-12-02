728e6713197d37313776764b75f5f980c58af026
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-12-02 19:07:00 +0900

    1, root_pathでグループが表示されるように@groupsを定義。 2. メッセージ投稿後、グループ詳細画面に遷移するようにした。 3. メッセージ投稿時group_idとuser_idが保存されるようにした。

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,20 +1,22 @@
 class ChatsController < ApplicationController
   def index
     @message = Message.new
+    @groups = Group.all
   end
 
   def create
     @message = Message.new(create_params)
+    @group_id = params.require(:message).permit(:group_id)[:group_id]
     if @message.save
-      redirect_to root_path
+      redirect_to group_path(@group_id)
     else
       flash[:alert] = "メッセージを入力してください"
-      redirect_to root_path
+      redirect_to group_path(@group_id)
     end
   end
 
   private
   def create_params
-      params.require(:message).permit(:body)
+      params.require(:message).permit(:body, :group_id).merge(user_id: current_user.id)
   end
 end
