35ff6e437dc4d031d210577410e9a1fe44d32d43
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-12-17 17:48:24 +0900

    ユーザー登録APIのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "chats#index"
   resources :chats, only: [:index, :create]
-  resources :groups, only: [:new, :create, :edit, :update, :show]
+  resources :groups, only: [:new, :create, :edit, :update, :show] do
+    collection do
+      get 'search'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
