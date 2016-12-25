4ba37019f59ac2de33c869256797ccd05a091d9d
猪狩蓮汰朗 <yuzukkouuuuuu@gmail.com>
2016-12-25 17:25:06 +0900

    オブジェクトとしてimageを取り出せるように、FormDataを使った。

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -14,19 +14,17 @@ $(function() {
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var textField = $('#chat__main--footer--text-js');
-    var body = textField.val();
-    var path_elements = window.location.pathname.split("/");
-    var group_id = Number(path_elements[path_elements.length - 1]);
+    // var body = textField.val();
+    // var path_elements = window.location.pathname.split("/");
+    // var group_id = Number(path_elements[path_elements.length - 1]);
+    var fd = new FormData($('form#new_message').get(0));
     $.ajax({
       type: 'POST',
       url: '/chats.json',
-      data: {
-        message: {
-          body: body,
-          group_id: group_id
-        }
-      },
-      dataType: 'json'
+      data: fd,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
      .done(function(data) {
        var html = buildHTML(data);
@@ -65,7 +63,7 @@ $(function() {
 });
 
 function buildHTML(message) {
-  var html = $(`<li><div class="chat__main--body--block"><div class="chat__main--body--block--name">${message.user_name}</div><div class="chat__main--body--block--date">${message.created_at}</div></div><p class="chat__main--body--message">${message.body}</p></li>`);
+  var html = $(`<li><div class="chat__main--body--block"><div class="chat__main--body--block--name">${message.user_name}</div><div class="chat__main--body--block--date">${message.created_at}</div></div><p class="chat__main--body--message">${message.body}</p><img src="${message.image}"></li>`);
   return html;
 };
 
